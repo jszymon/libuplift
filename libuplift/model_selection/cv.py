f1ae806e6cfa0a51176679bb5b3263dcdb188184
Szymon Jaroszewicz <jszymon@gmail.com>
2026-05-14 16:40:53 +0200

    Add y_stratify argument to uplift_check_cv Improve doc and comments

--- libuplift/model_selection/cv.py
+++ libuplift/model_selection/cv.py
@@ -1,21 +1,25 @@
 from sklearn.model_selection import check_cv
 from sklearn.preprocessing import LabelEncoder
 
-def uplift_check_cv(cv, y, trt, n_trt, *, classifier=False):
-    """Return a correct cv and y_stratify.
+def uplift_check_cv(cv, y, trt, n_trt, *, classifier=False, y_stratify=None):
+    """Return a correct crossvalidator cv and stratification target
+    y_stratify.
     
-    y_stratify may be used for stratification.  By default stratification
-    is done based on treatment for regression and based on cross of
-    treatment and target for classification.
+    By default the returned stratification target is the treatment for
+    regression and cross of treatment and target for classification.
+    If y_stratify is provided it is used instead and returned
+    unchanged.
+
     """
     
-    # always stratify on treatment and, if available, also on class
-    if classifier:
-        le = LabelEncoder()
-        y_stratify = le.fit_transform(y)
-        y_stratify = y_stratify * (n_trt+1) + trt
-    else:
-        y_stratify = trt
+    if y_stratify is None:
+        # always stratify on treatment and, if needed, also on class
+        if classifier:
+            le = LabelEncoder()
+            y_stratify = le.fit_transform(y)
+            y_stratify = y_stratify * (n_trt+1) + trt
+        else:
+            y_stratify = trt.copy()
     # classifier=True ensures stratification
     cv = check_cv(cv, y_stratify, classifier=True)
     return cv, y_stratify
